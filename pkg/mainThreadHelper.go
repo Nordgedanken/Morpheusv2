aa0897f24a25269e7bc4db7caf952c8d35169678
Marcel <MTRNord@users.noreply.github.com>
2018-05-01 18:37:17 +0200

    Fix CI (#3)
    * Make sure we used the installed go files instead of the ones from vendor
    
    * Fix copying
    
    * Fix linter and QT Source Copy
    
    * Debug whats happening inside the qtSrc Copy
    
    * Remove QT Src before copy
    
    * Remove ls call that broke the CI
    
    * Make linter happy
    
    * Keep QT outside of Dep
    
    * Ignore qt in dep
    
    * Fix ignored
    
    * Readd lock
    
    * Fix deb building
    
    * Fix early exit

--- pkg/mainThreadHelper.go
+++ pkg/mainThreadHelper.go
@@ -27,7 +27,7 @@ func Main() {
 // queue of work to run in main thread.
 var mainfunc = make(chan func())
 
-// do runs f on the main thread.
+// Do runs f on the main thread.
 func Do(f func()) {
 	done := make(chan bool, 1)
 	mainfunc <- func() {
