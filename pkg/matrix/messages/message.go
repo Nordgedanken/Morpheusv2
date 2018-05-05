6bc6fb565189576660410d839b5b0c4f0cf9c620
MTRNord <mtrnord1@gmail.com>
2018-05-05 21:13:02 +0200

    Implement Message (without the Show function)

--- pkg/matrix/messages/message.go
+++ pkg/matrix/messages/message.go
@@ -19,49 +19,67 @@ import (
 	"time"
 )
 
+// Message holds the needed Message data and allows to work with that. It gets normally loaded from the cache
 type Message struct {
+	eventID   string
+	event     *gomatrix.Event
+	authorID  string
+	text      string
+	timestamp *time.Time
 }
 
+// SetEventID adds the eventID to the current Message
 func (m *Message) SetEventID(id string) {
-
+	m.eventID = id
 }
 
+// SetEvent adds the gomatrix Event to the current Message
 func (m *Message) SetEvent(event *gomatrix.Event) {
-
+	m.event = event
 }
 
+// SetAuthorMXID adds the author MXID to the current Message
 func (m *Message) SetAuthorMXID(mxid string) {
-
+	m.authorID = mxid
 }
 
+// SetMessage adds the message Text to the current Message
 func (m *Message) SetMessage(message string) {
-
+	m.text = message
 }
 
+// SetTimestamp adds the Timestamp to the current Message
 func (m *Message) SetTimestamp(ts *time.Time) {
-
+	m.timestamp = ts
 }
 
+// GetEventID returns the event ID from the current Message
 func (m *Message) GetEventID() (id string) {
-	return ""
+	return m.eventID
 }
 
+// GetEvent returns the gomatrix event from the current Message
 func (m *Message) GetEvent() (event *gomatrix.Event) {
-	return nil
+	return m.event
 }
 
+// GetAuthorMXID returns the authors mxid from the current Message
 func (m *Message) GetAuthorMXID() (mxid string) {
-	return ""
+	return m.authorID
 }
 
+// GetMessage returns the text from the current Message
 func (m *Message) GetMessage() (message string) {
-	return ""
+	return m.text
 }
 
+// GetTimestamp returns the timestamp from the current Message
 func (m *Message) GetTimestamp() (ts *time.Time) {
-	return nil
+	return m.timestamp
 }
 
+// Show adds the current Message to the UI
+// TODO implement it
 func (m *Message) Show() error {
 	return nil
 }
